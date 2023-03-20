2c7070c12903edcd02510d3b89e3997ba3878b7d
Aakash8237 <127815306+Aakash8237@users.noreply.github.com>
2023-03-20 12:00:47 +1100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:hello_world/main.dart';
-import 'package:bitrise_flutter/main.dart';
-
 
 
 void main() {
